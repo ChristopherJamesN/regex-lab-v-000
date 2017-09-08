931976aaf3b70c4aca36858206ae96efeb98cca4
Chris Nady <nady.christopher@gmail.com>
2017-09-08 18:25:33 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  new_text=text.scan(/\w+un\w+ing/)
+  new_text=text.scan(/\bun\wing\b/)
 end
 
 def words_five_letters_long(text)
