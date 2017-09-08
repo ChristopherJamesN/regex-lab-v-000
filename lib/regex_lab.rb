07ea574ea190e1a80e247a3188c1191a2cd6b95c
Chris Nady <nady.christopher@gmail.com>
2017-09-08 18:14:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.each do |i|
   if i.scan(/\A[aeiou]/) == []
     return false
   else
     return true
   end
-end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
